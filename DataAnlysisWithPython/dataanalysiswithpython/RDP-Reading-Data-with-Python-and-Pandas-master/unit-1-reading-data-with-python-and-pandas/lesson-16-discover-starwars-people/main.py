ca8b2f97a156fe28470506216b45d45d25fb4831
Muchukota-Shiva <500091913@stu.upes.ac.in>
2025-06-22 19:04:15 +0530

    did request based data nalysis

--- DataAnlysisWithPython/dataanalysiswithpython/RDP-Reading-Data-with-Python-and-Pandas-master/unit-1-reading-data-with-python-and-pandas/lesson-16-discover-starwars-people/main.py
+++ DataAnlysisWithPython/dataanalysiswithpython/RDP-Reading-Data-with-Python-and-Pandas-master/unit-1-reading-data-with-python-and-pandas/lesson-16-discover-starwars-people/main.py
@@ -2,3 +2,25 @@ import numpy as np
 import pandas as pd
 import requests
 
+# r = requests.get("https://swapi.co/api/people/?format=json")
+
+# print(r.status_code,"\n")
+
+# swapi = r.json()
+# print(swapi)
+
+
+url = "https://swapi.dev/api/people/?format=json"
+response = requests.get(url, verify=False)
+
+if response.status_code == 200:
+    data = response.json()
+
+    starwars_people_df = pd.DataFrame(data['results'])
+    print("All people:\n", starwars_people_df[['name', 'eye_color']])
+
+
+    blue_eyed_people_df = starwars_people_df[starwars_people_df['eye_color'].str.contains('blue', case=False)]
+    print("\nBlue-eyed people:\n", blue_eyed_people_df[['name', 'eye_color']])
+else:
+    print("Failed to fetch data. Status code:", response.status_code)
