402291dbe30fe568c8be1aefff3a97858f672b49
Muchukota-Shiva <500091913@stu.upes.ac.in>
2025-06-24 11:14:31 +0530

    done, now for projects

--- DataAnlysisWithPython/dataanalysiswithpython/RDP-Reading-Data-with-Python-and-Pandas-master/unit-1-reading-data-with-python-and-pandas/lesson-18-get-fifa-players-from-the-web/main.py
+++ DataAnlysisWithPython/dataanalysiswithpython/RDP-Reading-Data-with-Python-and-Pandas-master/unit-1-reading-data-with-python-and-pandas/lesson-18-get-fifa-players-from-the-web/main.py
@@ -1,4 +1,14 @@
 import numpy as np
 import pandas as pd
 import requests
+import html5lib
 
+dfs = pd.read_html('files/fifa_players.html', encoding='utf-8')
+
+fifa_df = dfs[0]
+
+fifa_df = fifa_df.iloc[:, 2:-1]
+
+most_hits_player = fifa_df.sort_values('Hits', ascending=False).head(1)
+
+print(most_hits_player)
--- DataAnlysisWithPython/dataanalysiswithpython/RDP-Reading-Data-with-Python-and-Pandas-master/unit-1-reading-data-with-python-and-pandas/lesson-18-get-fifa-players-from-the-web/solutions/solution_fifa.py
+++ DataAnlysisWithPython/dataanalysiswithpython/RDP-Reading-Data-with-Python-and-Pandas-master/unit-1-reading-data-with-python-and-pandas/lesson-18-get-fifa-players-from-the-web/solutions/solution_fifa.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import requests
 
-dfs = pd.read_html('fifa_players.html', encoding='utf-8')
+dfs = pd.read_html('files/fifa_players.html', encoding='utf-8', flavor='html5lib')
 
 fifa_df = dfs[0]
 
